980c8be234e78e07f25f8d16c4e4a881f6b98e1c
Kovacs_Roland_Gergo <kovaxrawland@mailbox.unideb.hu>
2025-08-15 13:46:28 +0300

    Created basic CI pipeline and created a basic test folder

---
+++ tests/main_test.py
@@ -0,0 +1,16 @@
+import unittest
+from fastapi import testclient
+from src.app.main import app
+
+client=testclient.TestClient(app)
+
+class MyTestCase(unittest.TestCase):
+    def test_main(self):
+        main_response=client.get('/healthz')
+        self.assertEqual(main_response.json(),{
+            "status":"ok",
+            "status_code":200
+        })
+
+if __name__ == '__main__':
+    unittest.main()
